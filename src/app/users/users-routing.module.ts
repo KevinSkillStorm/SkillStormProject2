04488ad73c74a600ceea5a62f30787c42c45e4a2
BrandonVanek <bvanek@skillstorm.com>
2022-09-04 22:39:41 -0400

    adding guard to user/:id

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -4,13 +4,14 @@ import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ViewComponent } from './view/view.component';
+import { MsalGuard } from '@azure/msal-angular';
 
 const routes: Routes = [
   { path: 'users', redirectTo: 'users/index', pathMatch: 'full' },
   { path: 'users/index', component: IndexComponent },
   { path: 'users/sign-up', component: SignUpComponent },
   { path: 'users/login', component: LoginComponent },
-  { path: 'users/:id', component: ViewComponent },
+  { path: 'users/:id', component: ViewComponent, canActivate: [MsalGuard]},
 
 ];
 
