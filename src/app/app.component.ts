2adb8d81db6d6369c6cb925681bc71f4727ef17a
kyi0113 <KevinYi011@gmail.com>
2022-09-05 10:56:42 -0400

    Revert "changed buttons to anchor app.component.html"
    This reverts commit bc3974a7b288a929f5507ff3c252c64777ee890c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { CryptoUtils, Logger } from 'msal';
 import { UsersService } from './users/users.service';
 import { UserDTO } from './users/users';
 import { AppService } from './app.service';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -24,7 +23,6 @@ export class AppComponent {
   currentUserId: number = -1;
 
   constructor(
-    private router: Router,
     private sendEvent: AppService,
     private broadcastService: MsalBroadcastService,
     private authService: MsalService,
@@ -107,9 +105,6 @@ export class AppComponent {
   }
   public logout() {
     this.authService.logout();
-  }
-  public register(){
-    this.router.navigateByUrl('users/sign-up');
   }
 
 }
