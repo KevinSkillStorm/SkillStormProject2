9c2666e129b64b7cdc1a78dc1256c90989125fd7
kyi0113 <KevinYi011@gmail.com>
2022-09-07 08:12:46 -0400

    Minor changes to user page button.

--- src/app/users/index/index.component.ts
+++ src/app/users/index/index.component.ts
@@ -17,7 +17,7 @@ export class IndexComponent implements OnInit {
 
   loggedIn! : Boolean;
 
-
+  showCondition: Boolean = false;
 
   // @Input() currentUserId!: number;
 
@@ -33,19 +33,15 @@ export class IndexComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.app.checkAccount();
-    this.loggedIn = this.app.loggedIn;
-    console.log(`this.loggedIn = ${this.loggedIn}`);
-    console.log(`this.app.loggedIn = ${this.app.loggedIn}`);
-    
-    // this.currentUserId = this.sendEvent.getCurrentUserId();
-    
     setTimeout(() => {
       this.currentUserId = this.app.currentUserId;
       console.log(`this.currentUserId = ${this.currentUserId}`);
+      this.showCondition = true;
     }, 1000);
-    // console.log(`this.currentUserId = ${this.currentUserId}`);
-    
+    this.app.checkAccount();
+    this.loggedIn = this.app.loggedIn;
+    console.log(`this.loggedIn = ${this.loggedIn}`);
+    console.log(`this.app.loggedIn = ${this.app.loggedIn}`);    
   }
   ngOnChanges(): void {
     this.app.checkAccount();
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://victorious-sea-08c9bd610.1.azurestaticapps.net/api',
-  // apiUrl: 'https://localhost:4200/api',
+  // apiUrl: 'https://victorious-sea-08c9bd610.1.azurestaticapps.net/api',
+  apiUrl: 'https://localhost:4200/api',
 };
